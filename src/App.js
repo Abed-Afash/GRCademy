f9dbf49c6f401f052f9c1f41bdbbd8ba96dc669e
Abdalsamee Afash <abdalsamee.af@gmail.com>
2024-05-25 12:14:55 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import LandingPage from 'components/LandingPage';
+import GlobalStyles from 'styles/GlobalStyles';
+import { css } from "styled-components/macro"; //eslint-disable-line
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <GlobalStyles/>
+    <LandingPage/>
+    </>
+  )
 }
 
 export default App;
